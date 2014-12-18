dd88ac8b3a1d0f3438c0c951be812936a7e276bb
Cyriac Kandoth <ckandoth@gmail.com>
2014-12-17 22:17:30 -0500

    Better test MAF; Use X as the splice site AA

--- vcf2maf.pl
+++ vcf2maf.pl
@@ -524,7 +524,7 @@ while( my $line = $vcf_fh->getline ) {
             # Provide an AA position in HGVSp for splice acceptor/donor variants (HGVS deviant)
             if( $effect{Consequence} =~ m/^(splice_acceptor_variant|splice_donor_variant)$/ ) {
                 my ( $c_pos ) = $effect{HGVSc} =~ m/^c.(\d+)/;
-                $effect{HGVSp} = sprintf( "p.%.0f_splice", ( $c_pos + $c_pos % 3 ) / 3 ) if( defined $c_pos );
+                $effect{HGVSp} = sprintf( "p.X%.0f_splice", ( $c_pos + $c_pos % 3 ) / 3 ) if( defined $c_pos );
             }
 
             # Create a separate HGVS protein format using 1-letter codes
@@ -603,7 +603,7 @@ while( my $line = $vcf_fh->getline ) {
                 # Provide an AA position in HGVSp for splice acceptor/donor variants (HGVS deviant)
                 if( $effect{Effect} =~ m/^(splice_acceptor_variant|splice_donor_variant)$/ ) {
                     my ( $c_pos ) = $effect{HGVSc} =~ m/^c.(\d+)/;
-                    $effect{HGVSp} = sprintf( "p.%.0f_splice", ( $c_pos + $c_pos % 3 ) / 3 ) if( defined $c_pos );
+                    $effect{HGVSp} = sprintf( "p.X%.0f_splice", ( $c_pos + $c_pos % 3 ) / 3 ) if( defined $c_pos );
                 }
 
                 # Create a separate HGVS protein format using 1-letter codes
